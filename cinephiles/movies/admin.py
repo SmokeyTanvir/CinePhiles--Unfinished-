a7e5b64462e3b88acb9a901301876d8f0918bfe2
Tanvirul Islam <mazumdersaiful1973@gmail.com>
2023-08-15 20:54:51 +0600

    jani na

--- cinephiles/movies/admin.py
+++ cinephiles/movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models 
 
 # Register your models here.
+admin.site.register(models.Genre)
+admin.site.register(models.Actor)
+admin.site.register(models.Movie)
+admin.site.register(models.Role)
+admin.site.register(models.Review)
--- cinephiles/movies/models.py
+++ cinephiles/movies/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+    thumbnail = models.ImageField(upload_to='thumbnails/', null=True)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    thumbnail = models.ImageField(upload_to='thumbnails/', null=True)
+    release_date = models.DateField()
+    duration = models.PositiveIntegerField()  # Duration in minutes
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    genres = models.ManyToManyField(Genre)
+    description = models.TextField()
+    trailer_link = models.URLField(null=True)
+    downloadable_links = models.JSONField(null=True)  # Storing links as JSON
+    cast = models.ManyToManyField(Actor, through='Role')  # Use a through model for the role of actors
+
+    def __str__(self):
+        return self.title
+
+class Role(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    role_name = models.CharField(max_length=100)  # Name of the role played by the actor
+
+    def __str__(self):
+        return f"{self.actor.name} as {self.role_name} in {self.movie.title}"
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    reviewer_name = models.CharField(max_length=100)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"Review for {self.movie.title} by {self.reviewer_name}"
